3e2fb5306b7c7142ceae2efe2462cf6d740a95d9
Debashis <ishan.rath@gmail.com>
2017-03-12 11:07:25 +0530

    sd

--- wp-content/themes/customizr-pro/index.php
+++ wp-content/themes/customizr-pro/index.php
@@ -17,7 +17,7 @@ if ( apply_filters( 'czr_four_do', false ) ) {
 <div id="main-wrapper" class="<?php echo implode(' ', apply_filters( 'tc_main_wrapper_classes' , array('container') ) ) ?>">
     <?php if ( is_front_page() ) { ?>
     <div class="">
-       <h2 class="scom-h2-home-tag" style="text-align: center;font-size: 28px;color: #eb1d1f;font-weight: 700;">We provide solutions for your problems</h2>
+       <h2 class="scom-h2-home-tag">We provide solutions for your problems</h2>
     </div>
 <?php } ?>
     <?php do_action( '__before_main_container' ); ##hook of the featured page (priority 10) and breadcrumb (priority 20)...and whatever you need! ?>
@@ -25,8 +25,7 @@ if ( apply_filters( 'czr_four_do', false ) ) {
     <div class="container" role="main">
 <?php if ( is_front_page() ) { ?>
     <div class="">
-       <h3 class="scom-h2-home-tag" style="text-align: center;font-size: 28px;color: #eb1d1f;font-weight: 700;">
-       Our Products </h2>
+       <h3 class="scom-h2-home-tag">Our Products</h2>
     </div>
 <?php } ?>
 
@@ -70,24 +69,93 @@ if ( apply_filters( 'czr_four_do', false ) ) {
 
 <?php if ( is_front_page() ) { ?>
     <div class="">
-       <h3 class="scom-h2-home-tag" style="text-align: center;font-size: 28px;color: #eb1d1f;font-weight: 700;">Our Videos</h2>
+       <h3 class="scom-h2-home-tag" style="">Our Videos</h2>
     </div>
 
 <div class="row">
   <div class="span12">
     Level 1 column
     <div class="row">
-      <div class="span4">
+        <div class="span4">
             <iframe width="504" height="306" src="https://www.youtube.com/embed/rAkhFkrF_yc?feature=oembed" frameborder="0" allowfullscreen="" style="width: 100%;"></iframe>
         </div>
-      <div class="span4"> 
-      <iframe width="504" height="306" src="https://www.youtube.com/embed/Kc5iWa-Ky30?feature=oembed" frameborder="0" allowfullscreen="" style="width: 100%;"></iframe>
-      </div>
-      <div class="span4"><iframe width="504" height="306" src="https://www.youtube.com/embed/MbDndJdRi1o?feature=oembed" frameborder="0" allowfullscreen=""></iframe></div>
+        <div class="span4"> 
+             <iframe width="504" height="306" src="https://www.youtube.com/embed/Kc5iWa-Ky30?feature=oembed" frameborder="0" allowfullscreen="" style="width: 100%;"></iframe>
+        </div>
+        <div class="span4">
+             <iframe width="504" height="306" src="https://www.youtube.com/embed/MbDndJdRi1o?feature=oembed" frameborder="0" allowfullscreen=""></iframe>
+        </div>
     </div>
-    <p><a href="">View More</a></p>
+    <p class="home_view_more"><a href="">View More</a></p>
   </div>
 </div>
+
+
+ <div class="row main-testimonial">
+ <h4 class="peopleabout scom-h2-home-tag">What people say about us</h4>
+    <div class="span12">
+        <div class="mySlides">
+             <img src="wp-content/themes/customizr-pro/assets/front/img/male.png" alt="male" width="80px" height="80px"> 
+             <p class="testimonial-description"><i class="fa fa-quote-left" aria-hidden="true"></i> Hypnosis under clinical conditions, is a great wonderful tool to deal with trauma, behavioral issues, relationship issues, anxiety, depression, insomnia, skin rashes,lack of motivation, examination phobia, fear of rejection <i class="fa fa-quote-right" aria-hidden="true"></i> <br>
+             <span class="persionname">Ishan,Bangalore</span></p>  
+        </div>
+        <div class="mySlides"> 
+             <img src="wp-content/themes/customizr-pro/assets/front/img/male.png" alt="male" width="80px" height="80px">
+             <p class="testimonial-description"><i class="fa fa-quote-left" aria-hidden="true"></i> Hypnosis under clinical conditions, is a great wonderful tool to deal with trauma, behavioral issues, relationship issues, anxiety, depression, insomnia, skin rashes,lack of motivation, examination phobia, fear of rejection <i class="fa fa-quote-right" aria-hidden="true"></i><br>
+             <span class="persionname">Ishan,Bangalore</span></p>  
+        </div>  
+        <div class="mySlides"> 
+            <img src="wp-content/themes/customizr-pro/assets/front/img/male.png" alt="male" width="80px" height="80px"> 
+            <p class="testimonial-description"><i class="fa fa-quote-left" aria-hidden="true"></i> Hypnosis under clinical conditions, is a great wonderful tool to deal with trauma, behavioral issues, relationship issues, anxiety, depression, insomnia, skin rashes,lack of motivation, examination phobia, fear of rejection <i class="fa fa-quote-right" aria-hidden="true"></i><br>
+            <span class="persionname">Ishan,Bangalore</span></p>  
+        </div>
+     </div>
+</div>
+ <p class="home_view_more"><a href="">View More</a></p>
+ </div>
+
+<style type="text/css">
+.scom-h2-home-tag {
+  text-align: center;font-size: 28px;color: #eb1d1f;font-weight: 700;
+}
+h4.peopleabout.scom-h2-home-tag {
+    margin-top: 60px;
+    margin-bottom: 30px;
+}
+  .home_view_more {
+    float: right;
+    color: #ef0000;
+  }
+  .testimonial-description {
+    margin-top: 20px;
+    font-style: italic;
+  }
+  .persionname {float: center; color:#ef0000; margin-top: 20px;  font-style: normal;}
+  .home_view_more a {
+    color: #ef0000;
+    font-size: 16px;
+  }
+  .main-testimonial {
+    text-align: center;
+  }
+</style>
+
+<script>
+var myIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 7000); // Change image every 2 seconds
+}
+</script>
+
 <?php } ?>
 
 </div><!-- //#main-wrapper -->
