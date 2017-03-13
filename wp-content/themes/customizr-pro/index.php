82fd83bf408c42cba2fcfe808960264b42677356
Debashis <ishan.rath@gmail.com>
2017-03-13 16:12:50 +0530

    product page and video page live

--- wp-content/themes/customizr-pro/index.php
+++ wp-content/themes/customizr-pro/index.php
@@ -65,16 +65,13 @@ if ( apply_filters( 'czr_four_do', false ) ) {
         </div><!--.row -->
     </div><!-- .container role: main -->
 </div>
-    <?php do_action( '__after_main_container' ); ?>
+
+  <?php do_action( '__after_main_container' ); ?>
 
 <?php if ( is_front_page() ) { ?>
-    <div class="">
-       <h3 class="scom-h2-home-tag" style="">Our Videos</h2>
-    </div>
-
+<a href="videos"><h3 class="scom-h2-home-tag" style="">Our Videos</h3></a>
 <div class="row">
   <div class="span12">
-    Level 1 column
     <div class="row">
         <div class="span4">
             <iframe width="504" height="306" src="https://www.youtube.com/embed/rAkhFkrF_yc?feature=oembed" frameborder="0" allowfullscreen="" style="width: 100%;"></iframe>
@@ -86,60 +83,32 @@ if ( apply_filters( 'czr_four_do', false ) ) {
              <iframe width="504" height="306" src="https://www.youtube.com/embed/MbDndJdRi1o?feature=oembed" frameborder="0" allowfullscreen=""></iframe>
         </div>
     </div>
-    <p class="home_view_more"><a href="">View More</a></p>
+    <p class="home_view_more"><a href="videos">View More...</a></p>
   </div>
 </div>
-
-
- <div class="row main-testimonial">
+<div class="row main-testimonial">
  <h4 class="peopleabout scom-h2-home-tag">What people say about us</h4>
     <div class="span12">
         <div class="mySlides">
              <img src="wp-content/themes/customizr-pro/assets/front/img/male.png" alt="male" width="80px" height="80px"> 
-             <p class="testimonial-description"><i class="fa fa-quote-left" aria-hidden="true"></i> Hypnosis under clinical conditions, is a great wonderful tool to deal with trauma, behavioral issues, relationship issues, anxiety, depression, insomnia, skin rashes,lack of motivation, examination phobia, fear of rejection <i class="fa fa-quote-right" aria-hidden="true"></i> <br>
+             <p class="testimonial-description"><i class="fa fa-quote-left" aria-hidden="true"></i> Hypnosis under clinical conditions, is a great wonderful tool to deal with trauma, behavioral issues,hes,lack of motivation, examination phobia, fear of rejection <i class="fa fa-quote-right" aria-hidden="true"></i> <br>
              <span class="persionname">Ishan,Bangalore</span></p>  
         </div>
         <div class="mySlides"> 
              <img src="wp-content/themes/customizr-pro/assets/front/img/male.png" alt="male" width="80px" height="80px">
-             <p class="testimonial-description"><i class="fa fa-quote-left" aria-hidden="true"></i> Hypnosis under clinical conditions, is a great wonderful tool to deal with trauma, behavioral issues, relationship issues, anxiety, depression, insomnia, skin rashes,lack of motivation, examination phobia, fear of rejection <i class="fa fa-quote-right" aria-hidden="true"></i><br>
+             <p class="testimonial-description"><i class="fa fa-quote-left" aria-hidden="true"></i>  is a great wonderful tool to deal with trauma, behavioral issues, relationship issues, anxiety, depression, insomnia, skin rashes,lack of motivation, examination phobia, fear of rejection <i class="fa fa-quote-right" aria-hidden="true"></i><br>
              <span class="persionname">Ishan,Bangalore</span></p>  
         </div>  
         <div class="mySlides"> 
             <img src="wp-content/themes/customizr-pro/assets/front/img/male.png" alt="male" width="80px" height="80px"> 
-            <p class="testimonial-description"><i class="fa fa-quote-left" aria-hidden="true"></i> Hypnosis under clinical conditions, is a great wonderful tool to deal with trauma, behavioral issues, relationship issues, anxiety, depression, insomnia, skin rashes,lack of motivation, examination phobia, fear of rejection <i class="fa fa-quote-right" aria-hidden="true"></i><br>
+            <p class="testimonial-description"><i class="fa fa-quote-left" aria-hidden="true"></i> Hypnosis under , is a great wonderful tool to deal with trauma, behavioral issues, relationship issues, anxiety, depression, insomnia, skin rashes,lack of motivation, examination phobia, fear of rejection <i class="fa fa-quote-right" aria-hidden="true"></i><br>
             <span class="persionname">Ishan,Bangalore</span></p>  
         </div>
      </div>
 </div>
- <p class="home_view_more"><a href="">View More</a></p>
- </div>
+  
 
-<style type="text/css">
-.scom-h2-home-tag {
-  text-align: center;font-size: 28px;color: #eb1d1f;font-weight: 700;
-}
-h4.peopleabout.scom-h2-home-tag {
-    margin-top: 60px;
-    margin-bottom: 30px;
-}
-  .home_view_more {
-    float: right;
-    color: #ef0000;
-  }
-  .testimonial-description {
-    margin-top: 20px;
-    font-style: italic;
-  }
-  .persionname {float: center; color:#ef0000; margin-top: 20px;  font-style: normal;}
-  .home_view_more a {
-    color: #ef0000;
-    font-size: 16px;
-  }
-  .main-testimonial {
-    text-align: center;
-  }
-</style>
-
+<style type="text/css">  </style>
 <script>
 var myIndex = 0;
 carousel();
